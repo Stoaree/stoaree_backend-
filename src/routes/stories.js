c708662908db6bbe1d8883aceb838f22a5cc958d
Max Kelly <max@revelthat.com>
2020-01-20 16:13:40 +1100

    fixed app error. Changes stories routes from app to router

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 /* GET stories listing. */
 
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
   res.send("Here is where the list of stories will be");
 });
 
-app.get("/:id", function(req, res) {
+router.get("/:id", function(req, res) {
   res.send("Here is where a specific story id show page will be found");
 });
 
-app.post("/", function(req, res) {
+router.post("/", function(req, res) {
   res.send("Here is where a new posted story will be added to the list");
 });
 
