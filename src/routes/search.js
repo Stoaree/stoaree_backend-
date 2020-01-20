ab71024ca566507534ea89ca4114dbb39cde3e90
Max Kelly <max@revelthat.com>
2020-01-20 16:18:38 +1100

    fixed search routers

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', function (res, req) {
-  return res.send('Search request has been sent!')
+router.post('/', function (req, res) {
+  res.send('Search request has been sent!')
 });
 
-router.get('/', function (res, req) {
-  return res.send('search page');
+router.get('/', function (req, res) {
+  res.send('search page');
 });
 
 module.exports = router;
