018ede4a4fbf29869c1537aa45de7c011ede3c49
luke <l.j.dwsn@gmail.com>
2020-01-20 15:47:39 +1100

    stories routes

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET stories listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+
+app.get("/", function(req, res) {
+  res.send("Here is where the list of stories will be");
 });
 
-module.exports = router;
+app.get("/:id", async (req, res) => {
+  res.send("Here is where a specific story id show page will be found");
+});
+
+app.post("/", function(req, res) {
+  res.send("Here is where a new posted story will be added to the list");
+});
