260f45052d4f30c8560a667e7d5bfa1f093616e5
techspeterson <tpeterson.engsci@gmail.com>
2020-01-21 15:57:01 +1100

    user show route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require("../models/User");
+const Story = require("../models/Story");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get("/:id", async function (req, res) {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  const { firstName, lastName, displayName, location, avatarURL } = user;
+  const stories = await Story.find({ interviewer: id });
+
+  const userDisplayData = {
+    _id: id,
+    firstName,
+    lastName,
+    displayName,
+    location,
+    avatarURL,
+    stories
+  }
+
+  res.json(userDisplayData);
+})
 
 module.exports = router;
