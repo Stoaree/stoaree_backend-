a8a3724663e2ec5291b3eb096e8b6522f9bd42fa
techspeterson <tpeterson.engsci@gmail.com>
2020-01-21 16:41:42 +1100

    user controller

--- src/routes/search.js
+++ src/routes/search.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Story = require("../models/Story");
 
 router.post('/', async function (req, res) {
-  // res.send('Search request has been sent!')
+  // returns stories that contain search query in title/description/tags
   const { query } = req.body;
   const foundStories = await Story.find({ $or: [{ title: `/${query}/` }, { description: `/${query}/` }, { tags: query }] });
   res.json(foundStories);
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,38 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require("../models/User");
+const { createUser } = require("../controllers/user_controller");
 
-function sendError(res, err) {
-  res.status(400).send('Error: ' + err);
-}
-
-// Signup page
-router.get('/', function (req, res) {
-  res.send('This is the signup page')
-});
-
-router.post('/', async function (req, res) {
-  // res.send('You have posted to the sign up page')
-  const { email, firstName, lastName, password, displayName, dateOfBirth, location, avatarURL } = req.body;
-
-  // make sure to hash password before saving
-  const user = new User({
-    email,
-    firstName,
-    lastName,
-    password,
-    displayName,
-    dateOfBirth,
-    location,
-    avatarURL
-  });
-
-  try {
-    const savedUser = await user.save();
-    res.json(savedUser);
-  }
-  catch (err) { sendError(res, err); }
-});
+router.post('/', createUser);
 
 module.exports = router;
