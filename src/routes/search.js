1709c1b91c3270a303ae724dde11363e0b7505bb
techspeterson <tpeterson.engsci@gmail.com>
2020-01-21 12:26:32 +1100

    search function

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', function (req, res) {
-  res.send('Search request has been sent!')
+const Story = require("../models/Story");
+
+router.post('/', async function (req, res) {
+  // res.send('Search request has been sent!')
+  const { query } = req.body;
+  const foundStories = await Story.find({ $or: [{ title: `/${query}/` }, { description: `/${query}/` }, { tags: query }] });
+  res.json(foundStories);
 });
 
 router.get('/', function (req, res) {
