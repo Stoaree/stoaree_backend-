989e4e974d01598a3f6d46af21c380fcf0dbb8a0
techspeterson <tpeterson.engsci@gmail.com>
2020-01-21 11:43:24 +1100

    comment routes

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,20 +1,68 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get("/:story_id", (req, res) => {
-  res.send("This will get the comments for a particular story");
+const Comment = require("../models/Comment");
+const Story = require("../models/Story");
+
+router.get("/:story_id", async (req, res) => {
+  // res.send("This will get the comments for a particular story");
+  try {
+    const story = await Story.findById(req.params.story_id);
+    res.json(story.comments);
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
-router.post("/:story_id", (req, res) => {
-  res.send("This will add a new comment to a story");
+router.post("/:story_id", async (req, res) => {
+  // res.send("This will add a new comment to a story");
+  const { user, comment } = req.body; // get user from auth token instead?
+  const newComment = new Comment({
+    user,
+    comment
+  });
+
+  try {
+    const savedComment = await newComment.save();
+    const story = await Story.findById(req.params.story_id);
+    story.comment.push(savedComment);
+    story.save();
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
 router.put("/:story_id/:comment_id", (req, res) => {
-  res.send("This will edit a comment");
+  // res.send("This will edit a comment");
+  const { story_id, comment_id } = req.params;
+  const { comment } = req.body;
+
+  try {
+    const story = Story.findById(story_id);
+    if (story.comments.includes(comment_id)) {
+      const commentObj = Comment.findById(comment_id);
+      commentObj.comment = comment;
+      await commentObj.send();
+      res.json(commentObj);
+    }
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
-router.delete("/:story_id/:comment_id", (req, res) => {
-  res.send("This will delete a comment");
+router.delete("/:story_id/:comment_id", async (req, res) => {
+  // res.send("This will delete a comment");
+  const { story_id, comment_id } = req.params;
+
+  try {
+    const story = Story.findById(story_id);
+    if (story.comments.includes(comment_id)) {
+      const comment = Comment.findById(comment_id);
+      await comment.remove();
+      const index = story.comments.indexOf(comment_id);
+      story.comments.splice(index, 1);
+      await story.save();
+      res.json(story.comments);
+    }
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
 module.exports = router;
--- src/routes/stories.js
+++ src/routes/stories.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const Story = require("../models/Story");
 
-/* GET stories listing. */
+const Story = require("../models/Story");
 
 router.get("/", async function (req, res) {
   // res.send("Here is where the list of stories will be");
-  const stories = await Story.find();
-  res.json(stories);
+  try {
+    const stories = await Story.find();
+    res.json(stories);
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
 router.get("/:id", async function (req, res) {
   // res.send("Here is where a specific story id show page will be found");
-  const story = await Story.findById(req.params.id);
-  res.json(story);
+  try {
+    const story = await Story.findById(req.params.id);
+    res.json(story);
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
 router.post("/", async function (req, res) {
   // res.send("Here is where a new posted story will be added to the list");
-  const { title, description, interviewer, interviewee, tags, comments, questions } = req.body;
-  const newStory = new Story({ title, description, interviewer, interviewee, tags, comments, questions });
-  const savedStory = await newStory.save();
-  res.json(savedStory);
+  const { title, description, interviewer, interviewee, tags, questions } = req.body;
+  const newStory = new Story({
+    title,
+    description,
+    interviewer,
+    interviewee,
+    tags,
+    comments: [],
+    questions
+  });
+
+  try {
+    const savedStory = await newStory.save();
+    res.json(savedStory);
+  }
+  catch (err) { res.status(400).send('Error: ' + err); }
 });
 
 module.exports = router;
